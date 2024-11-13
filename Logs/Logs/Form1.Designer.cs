7fca5d3bdb804361044d0f7add6d1ce7ee426377
nalfo15 <136972872+nalfo15@users.noreply.github.com>
2024-11-14 00:54:03 +0200

    Add files via upload

---
+++ Logs/Logs/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace Logs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.groupBoxJournaux = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBoxEvenement = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBoxBoutons = new System.Windows.Forms.GroupBox();
+            this.comboBoxFilterType = new System.Windows.Forms.ComboBox();
+            this.buttonQuitter = new System.Windows.Forms.Button();
+            this.buttonToutSuppr = new System.Windows.Forms.Button();
+            this.buttonSuppr = new System.Windows.Forms.Button();
+            this.buttonFiltrer = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.groupBoxJournaux.SuspendLayout();
+            this.groupBoxEvenement.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBoxBoutons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.30893F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 66.69107F));
+            this.tableLayoutPanel1.Controls.Add(this.groupBoxJournaux, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBoxEvenement, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.groupBoxBoutons, 1, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.86348F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.13652F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1386, 564);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // groupBoxJournaux
+            // 
+            this.groupBoxJournaux.Controls.Add(this.listBox1);
+            this.groupBoxJournaux.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxJournaux.Location = new System.Drawing.Point(3, 3);
+            this.groupBoxJournaux.Name = "groupBoxJournaux";
+            this.groupBoxJournaux.Size = new System.Drawing.Size(455, 444);
+            this.groupBoxJournaux.TabIndex = 0;
+            this.groupBoxJournaux.TabStop = false;
+            this.groupBoxJournaux.Text = "Journaux d\'évenement";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(3, 18);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(449, 423);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // groupBoxEvenement
+            // 
+            this.groupBoxEvenement.Controls.Add(this.dataGridView1);
+            this.groupBoxEvenement.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxEvenement.Location = new System.Drawing.Point(464, 3);
+            this.groupBoxEvenement.Name = "groupBoxEvenement";
+            this.groupBoxEvenement.Size = new System.Drawing.Size(919, 444);
+            this.groupBoxEvenement.TabIndex = 1;
+            this.groupBoxEvenement.TabStop = false;
+            this.groupBoxEvenement.Text = "Evenement";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Cursor = System.Windows.Forms.Cursors.UpArrow;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(913, 423);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBoxBoutons
+            // 
+            this.groupBoxBoutons.Controls.Add(this.comboBoxFilterType);
+            this.groupBoxBoutons.Controls.Add(this.buttonQuitter);
+            this.groupBoxBoutons.Controls.Add(this.buttonToutSuppr);
+            this.groupBoxBoutons.Controls.Add(this.buttonSuppr);
+            this.groupBoxBoutons.Controls.Add(this.buttonFiltrer);
+            this.groupBoxBoutons.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBoxBoutons.Location = new System.Drawing.Point(464, 453);
+            this.groupBoxBoutons.Name = "groupBoxBoutons";
+            this.groupBoxBoutons.Size = new System.Drawing.Size(919, 108);
+            this.groupBoxBoutons.TabIndex = 2;
+            this.groupBoxBoutons.TabStop = false;
+            this.groupBoxBoutons.Text = "Boutons";
+            // 
+            // comboBoxFilterType
+            // 
+            this.comboBoxFilterType.FormattingEnabled = true;
+            this.comboBoxFilterType.Location = new System.Drawing.Point(23, 21);
+            this.comboBoxFilterType.Name = "comboBoxFilterType";
+            this.comboBoxFilterType.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxFilterType.TabIndex = 4;
+            // 
+            // buttonQuitter
+            // 
+            this.buttonQuitter.Location = new System.Drawing.Point(390, 21);
+            this.buttonQuitter.Name = "buttonQuitter";
+            this.buttonQuitter.Size = new System.Drawing.Size(124, 62);
+            this.buttonQuitter.TabIndex = 3;
+            this.buttonQuitter.Text = "Quitter";
+            this.buttonQuitter.UseVisualStyleBackColor = true;
+            this.buttonQuitter.Click += new System.EventHandler(this.buttonQuitter_Click);
+            // 
+            // buttonToutSuppr
+            // 
+            this.buttonToutSuppr.Location = new System.Drawing.Point(266, 21);
+            this.buttonToutSuppr.Name = "buttonToutSuppr";
+            this.buttonToutSuppr.Size = new System.Drawing.Size(118, 62);
+            this.buttonToutSuppr.TabIndex = 2;
+            this.buttonToutSuppr.Text = "Tout supprimer";
+            this.buttonToutSuppr.UseVisualStyleBackColor = true;
+            this.buttonToutSuppr.Click += new System.EventHandler(this.buttonToutSuppr_Click);
+            // 
+            // buttonSuppr
+            // 
+            this.buttonSuppr.Location = new System.Drawing.Point(150, 21);
+            this.buttonSuppr.Name = "buttonSuppr";
+            this.buttonSuppr.Size = new System.Drawing.Size(110, 62);
+            this.buttonSuppr.TabIndex = 1;
+            this.buttonSuppr.Text = "Supprimer";
+            this.buttonSuppr.UseVisualStyleBackColor = true;
+            this.buttonSuppr.Click += new System.EventHandler(this.buttonSuppr_Click);
+            // 
+            // buttonFiltrer
+            // 
+            this.buttonFiltrer.AutoSize = true;
+            this.buttonFiltrer.Location = new System.Drawing.Point(20, 51);
+            this.buttonFiltrer.Name = "buttonFiltrer";
+            this.buttonFiltrer.Size = new System.Drawing.Size(124, 32);
+            this.buttonFiltrer.TabIndex = 0;
+            this.buttonFiltrer.Text = "Filtrer";
+            this.buttonFiltrer.UseVisualStyleBackColor = true;
+            this.buttonFiltrer.Click += new System.EventHandler(this.buttonFiltrer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1386, 564);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.groupBoxJournaux.ResumeLayout(false);
+            this.groupBoxEvenement.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBoxBoutons.ResumeLayout(false);
+            this.groupBoxBoutons.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.GroupBox groupBoxJournaux;
+        private System.Windows.Forms.GroupBox groupBoxEvenement;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBoxBoutons;
+        private System.Windows.Forms.Button buttonQuitter;
+        private System.Windows.Forms.Button buttonToutSuppr;
+        private System.Windows.Forms.Button buttonSuppr;
+        private System.Windows.Forms.Button buttonFiltrer;
+        private System.Windows.Forms.ComboBox comboBoxFilterType;
+    }
+}
+
---
+++ Logs/Logs/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Logs
+{
+    public partial class Form1 : Form
+    {
+        private List<EventLogEntry> eventEntries = new List<EventLogEntry>();
+
+        public Form1()
+        {
+            // Liste pour stocker les entrées de journal d'événements temporairement
+            InitializeComponent();
+
+            // Activer les boutons
+            buttonFiltrer.Enabled = true;
+            buttonSuppr.Enabled = true;
+            buttonToutSuppr.Enabled = true;
+            buttonQuitter.Enabled = true;
+
+            // Ajouter des options à la ComboBox de filtrage et sélectionner la première par défaut
+            comboBoxFilterType.Items.AddRange(new string[] { "Information", "Warning", "Error", "SuccessAudit", "FailureAudit" });
+            comboBoxFilterType.SelectedIndex = 0; // Sélection par défaut
+
+            // Charger les journaux et configurer DataGridView
+            MessageBox.Show("Chargement des journaux d'événements...");
+            LoadEventLogs();
+            InitializeDataGridView();
+        }
+
+        // Fonction pour charger les journaux d'événements disponibles
+        private void LoadEventLogs()
+        {
+            try
+            {
+                listBox1.Items.Clear();// Effacer les items dans la liste
+                foreach (var log in EventLog.GetEventLogs())
+                {
+                    try
+                    {
+                        var entryCount = log.Entries.Count; // Tester l'accès en lisant le nombre d'entrées
+                        listBox1.Items.Add(log.Log); // Utiliser le nom du journal
+                    }
+                    catch (System.Security.SecurityException)
+                    {
+                        // Message si l'accès est refusé pour un journal spécifique
+                        MessageBox.Show("Accès refusé pour le journal : " + log.LogDisplayName);
+                    }
+                    catch (Exception ex)
+                    {
+                        // Message pour toute autre erreur lors du chargement des journaux
+                        MessageBox.Show("Erreur lors du chargement des journaux : " + ex.Message);
+                    }
+                }
+
+                if (listBox1.Items.Count == 0)
+                {
+                    // Vérifier si aucun journal n'a été chargé
+                    MessageBox.Show("Aucun journal d'événements n'a été chargé. Vérifiez les permissions.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Message pour erreur critique lors du chargement
+                MessageBox.Show("Erreur critique lors du chargement des journaux : " + ex.Message);
+            }
+        }
+
+        // Initialisation du DataGridView avec les colonnes nécessaires
+        private void InitializeDataGridView()
+        {
+            //initialisation de la DatagridView
+            dataGridView1.Columns.Clear();
+            dataGridView1.Columns.Add("Niveau", "Niveau");
+            dataGridView1.Columns.Add("DateEtHeure", "Date et Heure");
+            dataGridView1.Columns.Add("Source", "Source");
+            dataGridView1.Columns.Add("Categorie", "Catégorie");
+            dataGridView1.Columns.Add("Message", "Message");
+
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            MessageBox.Show("DataGridView configuré avec les colonnes.");
+        }
+       
+        // Fonction déclenchée lors de la sélection d'un journal dans la liste
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                string selectedLog = listBox1.SelectedItem.ToString(); // Récupérer le nom du journal sélectionné
+                MessageBox.Show("Journal sélectionné : " + selectedLog);
+                LoadEvents(selectedLog);// Charger les événements pour le journal sélectionné
+            }
+        }
+
+        // Charger les événements d'un journal spécifique
+        private void LoadEvents(string logName)
+        {
+            if (dataGridView1.Columns.Count == 0)
+            {
+                InitializeDataGridView();
+            }
+
+            dataGridView1.Rows.Clear(); // Effacer les lignes existantes dans DataGridView
+            eventEntries.Clear(); // Effacer la liste temporaire
+
+            try
+            {
+                var log = new EventLog(logName);
+
+                foreach (EventLogEntry entry in log.Entries)
+                {
+                    // Ajouter chaque entrée à la liste temporaire
+                    eventEntries.Add(entry);
+
+                    // Ajouter chaque entrée au DataGridView pour affichage initial
+                    dataGridView1.Rows.Add(entry.EntryType, entry.TimeGenerated, entry.Source, entry.Category, entry.Message);
+                }
+
+                MessageBox.Show("Événements chargés pour le journal : " + logName);
+            }
+            catch (Exception ex)
+            {
+                // Message en cas d'erreur lors du chargement des événements
+                MessageBox.Show("Erreur lors du chargement des événements du journal '" + logName + "' : " + ex.Message);
+            }
+        }
+
+        // Fonction pour supprimer les lignes sélectionnées dans le DataGridView
+        private void buttonSuppr_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.Remove(row);
+            }
+        }
+
+        // Fonction pour supprimer toutes les lignes du DataGridView
+        private void buttonToutSuppr_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+        }
+       
+        // Fonction pour quitter l'application
+        private void buttonQuitter_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // Fonction pour filtrer les événements selon le type sélectionné dans la ComboBox
+        private void buttonFiltrer_Click(object sender, EventArgs e)
+        {
+            // Vérification si comboBoxFilterType ou SelectedItem est nul
+            if (comboBoxFilterType == null || comboBoxFilterType.SelectedItem == null)
+            {
+                MessageBox.Show("Erreur : Veuillez sélectionner un niveau de filtre dans la ComboBox.");
+                return;
+            }
+
+            // Récupérer le niveau de filtrage sélectionné dans la ComboBox
+            string filterType = comboBoxFilterType.SelectedItem.ToString();
+
+            // Effacer les lignes actuelles dans le DataGridView sans toucher à la liste d'origine
+            dataGridView1.Rows.Clear();
+
+            // Parcourir la liste temporaire et afficher uniquement les éléments correspondants
+            foreach (var entry in eventEntries)
+            {
+                if (entry.EntryType.ToString() == filterType)
+                {
+                    dataGridView1.Rows.Add(entry.EntryType, entry.TimeGenerated, entry.Source, entry.Category, entry.Message);
+                }
+            }
+
+            MessageBox.Show("Filtrage terminé pour le niveau : " + filterType);
+        }
+
+    }
+}
